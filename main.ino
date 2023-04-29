dd11fa46f8903bcceb234ba4a80ed9c5bee3544c
Aman <74829564+iamangangwar@users.noreply.github.com>
2023-04-29 17:04:00 +0530

    added files

---
+++ main.ino
@@ -0,0 +1,190 @@
+#include <ESP8266WiFi.h>
+#include <WiFiClientSecure.h>
+#include <PubSubClient.h>
+#include <ArduinoJson.h>
+#include <time.h>
+#include "secrets.h"
+#include "DHT.h"
+#include <Servo.h>
+ 
+#define DHTPIN 4        // Digital pin connected to the DHT sensor
+#define DHTTYPE DHT11   // DHT 11
+int red = 15;
+int green = 12;
+int yellow = 13;
+int buzzer = 2;
+ 
+DHT dht(DHTPIN, DHTTYPE);
+ 
+float h ;
+float t;
+unsigned long lastMillis = 0;
+unsigned long previousMillis = 0;
+const long interval = 5000;
+ 
+#define AWS_IOT_PUBLISH_TOPIC   "device/esp8266/data"
+#define AWS_IOT_SUBSCRIBE_TOPIC "device/+/data"
+ 
+WiFiClientSecure net;
+ 
+BearSSL::X509List cert(cacert);
+BearSSL::X509List client_crt(client_cert);
+BearSSL::PrivateKey key(privkey);
+ 
+PubSubClient client(net);
+ 
+time_t now;
+time_t nowish = 1510592825;
+ 
+Servo s1;  
+ 
+void NTPConnect(void)
+{
+  Serial.print("Setting time using SNTP");
+  configTime(TIME_ZONE * 3600, 0 * 3600, "pool.ntp.org", "time.nist.gov");
+  now = time(nullptr);
+  while (now < nowish)
+  {
+    delay(500);
+    Serial.print(".");
+    now = time(nullptr);
+  }
+  Serial.println("done!");
+  struct tm timeinfo;
+  gmtime_r(&now, &timeinfo);
+  Serial.print("Current time: ");
+  Serial.print(asctime(&timeinfo));
+}
+ 
+ 
+void messageReceived(char *topic, byte *payload, unsigned int length)
+{
+  Serial.print("Received [");
+  Serial.print(topic);
+  Serial.print("]: ");
+  for (int i = 0; i < length; i++)
+  {
+    Serial.print((char)payload[i]);
+  }
+  Serial.println();
+}
+ 
+ 
+void connectAWS()
+{
+  delay(3000);
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASSWORD);
+ 
+  Serial.println(String("Attempting to connect to SSID: ") + String(WIFI_SSID));
+ 
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+ 
+  NTPConnect();
+ 
+  net.setTrustAnchors(&cert);
+  net.setClientRSACert(&client_crt, &key);
+ 
+  client.setServer(MQTT_HOST, 8883);
+  client.setCallback(messageReceived);
+ 
+ 
+  Serial.println("Connecting to AWS IOT");
+ 
+  while (!client.connect(THINGNAME))
+  {
+    Serial.print(".");
+    delay(1000);
+  }
+ 
+  if (!client.connected()) {
+    Serial.println("AWS IoT Timeout!");
+    return;
+  }
+  // Subscribe to a topic
+  client.subscribe(AWS_IOT_SUBSCRIBE_TOPIC);
+ 
+  Serial.println("AWS IoT Connected!");
+}
+ 
+ 
+void publishMessage()
+{
+  StaticJsonDocument<200> doc;
+  doc["time"] = millis();
+  doc["humidity"] = h;
+  doc["temperature"] = t;
+  char jsonBuffer[512];
+  serializeJson(doc, jsonBuffer); // print to client
+ 
+  client.publish(AWS_IOT_PUBLISH_TOPIC, jsonBuffer);
+}
+ 
+ 
+void setup()
+{
+  Serial.begin(115200);
+  connectAWS();
+  dht.begin();
+  s1.attach(0);  // servo attach D3 pin of arduino  
+  pinMode(red, OUTPUT);
+  digitalWrite(red, LOW);
+  pinMode(green, OUTPUT);
+  digitalWrite(green, LOW);
+  pinMode(yellow, OUTPUT);
+  digitalWrite(yellow, HIGH);
+  pinMode(buzzer, OUTPUT);
+  digitalWrite(buzzer, LOW);
+}
+ 
+ 
+void loop()
+{
+  h = dht.readHumidity();
+  t = dht.readTemperature();
+ 
+  if (isnan(h) || isnan(t) )  // Check if any reads failed and exit early (to try again).
+  {
+    Serial.println(F("Failed to read from DHT sensor!"));
+    return;
+  }
+ 
+  Serial.print(F("Humidity: "));
+  Serial.print(h);
+  Serial.print(F("%  Temperature: "));
+  Serial.print(t);
+  Serial.println(F("°C "));
+  delay(1000);
+  digitalWrite(yellow, HIGH);
+  digitalWrite(red, LOW);
+  digitalWrite(green, HIGH);
+  if(t >= 33) {
+    s1.write(180);
+    digitalWrite(green, LOW);
+    digitalWrite(red, HIGH);
+    digitalWrite(buzzer, HIGH);
+  }
+  else {
+    s1.write(0);
+  }
+  
+  now = time(nullptr);
+ 
+  if (!client.connected())
+  {
+    connectAWS();
+  }
+  else
+  {
+    client.loop();
+    if (millis() - lastMillis > 5000)
+    {
+      lastMillis = millis();
+      publishMessage();
+    }
+  }
+}
